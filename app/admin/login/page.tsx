a0209fe493b03cf0024b320635bb9ac97c668181
João Murat <osteloniz30@gmail.com>
2026-01-27 21:09:18 -0300

    Ajuste loginpage

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import { LoginPage } from '@/components/admin/login-page'
 
 export default function AdminLoginPage() {
-  return <LoginPage />
+  return (
+    <Suspense fallback={null}>
+      <LoginPage />
+    </Suspense>
+  )
 }
